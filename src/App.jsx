983d4ac6d5a915a96b02910d6b9df01fc96c77d2
nicolaparutto <nicolaparutto2003@gmail.com>
2025-04-15 16:15:39 +0200

    feat: add form structure and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <section className="container">
+        <h1 className="title">Web Developer Signup</h1>
+
+        <form>
+          <div className="subscription-form">
+            <div className="form-left">
+              <label>Nome completo</label>
+              <input type="text" placeholder="Nome..." id="" />
+              <label>Username</label>
+              <input type="text" placeholder="Username..." id="" />
+              <label>Password</label>
+              <input type="password" placeholder="Password..." id="" />
+            </div>
+
+            <div className="form-right">
+              <label>Specializzazione</label>
+              <select>
+                <option>Scegli un'opzione</option>
+                <option>Full Stack</option>
+                <option>Frontend</option>
+                <option>Backend</option>
+              </select>
+              <label>Anni di esperienza</label>
+              <input type="number" />
+              <label>Breve descrizione personale</label>
+              <textarea></textarea>
+            </div>
+          </div>
+          <button type="submit">Iscriviti</button>
+        </form>
+
+      </section>
     </>
   )
 }
